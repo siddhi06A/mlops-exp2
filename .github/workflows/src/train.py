a0d63531774ad6d8a459067a1ff7b24bb8bde5ea
siddhi06A <143930395+siddhi06A@users.noreply.github.com>
2026-01-30 13:31:01 +0530

    Add training script for Logistic Regression model

---
+++ .github/workflows/src/train.py
@@ -0,0 +1,10 @@
+from sklearn.datasets import load_iris
+from sklearn.linear_model import LogisticRegression
+import joblib
+
+X, y = load_iris(return_X_y=True)
+model = LogisticRegression(max_iter=200)
+model.fit(X, y)
+
+joblib.dump(model, "model.pkl")
+print("Model trained successfully")
